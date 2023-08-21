533c41dd70159f557e681c2ec7f4a41e9c81e98b
Rafli Dewanto <raflidewanto97@gmail.com>
2023-08-21 14:28:15 +0700

    feat: skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import About from "@/components/about";
 import Divider from "@/components/divider";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
+import Skills from "@/components/skills";
 
 export default function Home() {
   return (
@@ -11,6 +12,8 @@ export default function Home() {
       <About />
       <Divider />
       <Projects />
+      <Divider />
+      <Skills />
     </main>
   );
 }
